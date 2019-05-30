d5c87d6f022369a3a9d83d5b365d52374bed0266
Jose Lee <lg282923166@gmail.com>
2019-05-31 00:23:43 +0800

    feat: add basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'hot/hotWidget.dart';
+import 'mine/mineWidget.dart';
+import 'movies/moviesWidget.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,94 +10,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Start Name Generator',
+      title: 'Flutter Demo',
       theme: new ThemeData(primaryColor: Colors.white),
-      home: new RandomWords(),
+      home: MyHomePage(title: '豆瓣电影'),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
   @override
-  createState() => new RandomWordsState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  void _pushSaved() {
-    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
-      final tiles = _saved.map(
-        (pair) {
-          return new ListTile(
-            title: new Text(
-              pair.asPascalCase,
-              style: _biggerFont,
-            ),
-          );
-        },
-      );
-      final divided = ListTile.divideTiles(
-        context: context,
-        tiles: tiles,
-      ).toList();
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Saved Suggestions'),
-        ),
-        body: new ListView(children: divided),
-      );
-    }));
-  }
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  final _widgetItems = [HotWidget(), MoviesWidget(), MineWidget()];
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Start Name Generator'),
-          actions: <Widget>[
-            new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
-          ],
-        ),
-        body: _buildSuggestions());
-  }
-
-  Widget _buildSuggestions() {
-    return new ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();
-
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
+    return Scaffold(
+      body: _widgetItems[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.school), title: Text('热映')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.panorama_fish_eye), title: Text('找片')),
+          BottomNavigationBarItem(icon: Icon(Icons.people), title: Text('我的'))
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
+      ),
     );
   }
 
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return new ListTile(
-      title: new Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
